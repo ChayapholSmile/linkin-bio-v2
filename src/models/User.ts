2f3ec4e0fb7f5b8ce7ade70f8e007c8c142e00a8
ChayapholSmile <151313831+ChayapholSmile@users.noreply.github.com>
2025-09-05 14:33:16 +0700

    Create User.ts

---
+++ src/models/User.ts
@@ -0,0 +1,17 @@
+// src/models/User.ts
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username."],
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password."],
+  },
+});
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
